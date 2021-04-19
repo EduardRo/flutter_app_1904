d680eafd0cb4c2b947a9763df476b436a3ec8ce9
EduardRo <eduardstancu@yahoo.com>
2021-04-19 23:48:36 +0300

    body modifications

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:applrn1904/constants.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
@@ -8,7 +9,16 @@ class Body extends StatelessWidget {
       Container(
         height: size.height * 0.2,
         child: Stack(
-          children: <Widget>[],
+          children: <Widget>[
+            Container(
+              height: size.height * 0.2 - 27,
+              decoration: BoxDecoration(
+                  color: kPrimaryColor,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(36),
+                      bottomRight: Radius.circular(36))),
+            )
+          ],
         ),
       )
     ]);
